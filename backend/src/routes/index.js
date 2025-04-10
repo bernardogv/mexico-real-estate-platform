51eaa817956de4f60c4206ece07cf50e29414655
Bernardo Gallegos-Vallejo <93843760+bernardogv@users.noreply.github.com>
2025-04-10 12:14:42 -0500

    Add routes index file for organized exports

---
+++ backend/src/routes/index.js
@@ -0,0 +1,11 @@
+const authRoutes = require('./auth.routes');
+const userRoutes = require('./user.routes');
+const propertyRoutes = require('./property.routes');
+const mediaRoutes = require('./media.routes');
+
+module.exports = {
+  authRoutes,
+  userRoutes,
+  propertyRoutes,
+  mediaRoutes
+};
