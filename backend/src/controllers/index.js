9dc74ee1d45344dd971004731380829253d94fdc
Bernardo Gallegos-Vallejo <93843760+bernardogv@users.noreply.github.com>
2025-04-10 12:13:50 -0500

    Add controller index file for organized exports

---
+++ backend/src/controllers/index.js
@@ -0,0 +1,11 @@
+const authController = require('./auth.controller');
+const userController = require('./user.controller');
+const propertyController = require('./property.controller');
+const mediaController = require('./media.controller');
+
+module.exports = {
+  authController,
+  userController,
+  propertyController,
+  mediaController
+};
